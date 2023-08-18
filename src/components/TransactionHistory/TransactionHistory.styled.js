4ab49d4db035fd1ac72a033078cb8e284195845f
Julipusik <yuliuaboyko@gmail.com>
2023-08-18 16:00:50 +0300

    updated transaction styles

--- src/components/TransactionHistory/TransactionHistory.styled.js
+++ src/components/TransactionHistory/TransactionHistory.styled.js
@@ -4,6 +4,7 @@ export const Table = styled.table`
     width: 50%;
     text-align: center;
     border-collapse: collapse;
+    border: 2px solid rgba(79, 46, 232, 0.40);
     margin-left: 16px;    
 
 
